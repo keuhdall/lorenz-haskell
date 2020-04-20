fe062d73cb2e7801c7652df1e21cc3d756337641
Léonard Marques <lmarques@student.42.fr>
2020-04-20 16:00:07 +0200

    fixed some equations

--- src/Attractor/Equations.hs
+++ src/Attractor/Equations.hs
@@ -5,62 +5,62 @@ import Control.Lens
 import Attractor.Data
 
 lorenzEquation :: SystemEquation
-lorenzEquation l = (dx, dy, dz) where
-  dx = (θ    * (l^.y  - l^.x)       ) * δt
-  dy = (l^.x * (ρ     - l^.z) - l^.y) * δt
-  dz = (l^.x *  l^.y  - ß     * l^.z) * δt
+lorenzEquation l = (dx*δt, dy*δt, dz*δt) where
+  dx = θ    * (l^.y  - l^.x)       
+  dy = l^.x * (ρ     - l^.z) - l^.y
+  dz = l^.x *  l^.y  - ß     * l^.z
   θ = 10
   ρ = 28
   ß = 8.0 / 3.0
 
 rosslerEquation :: SystemEquation
-rosslerEquation l = (dx, dy, dz) where
-  dx = -(l^.y - l^.z              ) * δt
-  dy =  (l^.x + a     * l^.y      ) * δt
-  dz =  (b    + l^.z  * (l^.x - c)) * δt
+rosslerEquation l = (dx*δt, dy*δt, dz*δt) where
+  dx = -(l^.y - l^.z)
+  dy =  l^.x  + a     * l^.y
+  dz =  b     + l^.z  * (l^.x - c)
   a = 0.2
   b = 0.2
   c = 5.7
 
 rikitakeEquation :: SystemEquation
-rikitakeEquation l = (dx, dy, dz) where
-  dx = -µ * l^.x + l^.z * l^.y * δt
-  dy = -µ * l^.y + l^.x * (l^.z - a) * δt
-  dz = 1 - l^.x * l^.y * δt
+rikitakeEquation l = (dx*δt, dy*δt, dz*δt) where
+  dx = -µ * l^.x + l^.z * l^.y
+  dy = -µ * l^.y + l^.x * (l^.z - a)
+  dz = 1  - l^.x * l^.y
   µ = 1.0
   a = 5.0
 
 noseHooverEquation :: SystemEquation
-noseHooverEquation l = (dx, dy, dz) where
-  dx = l^.y * δt
-  dy = l^.y * l^.z - l^.x * δt
-  dz = 1 - (l^.y) ** 2 * δt
+noseHooverEquation l = (dx*δt, dy*δt, dz*δt) where
+  dx =  l^.y
+  dy = -l^.x + l^.y * l^.z
+  dz =  1  * ((l^.y) ** 2)
 
 wangEquation :: SystemEquation
-wangEquation l = (dx, dy, dz) where
-  dx = l^.x - l^.y * l^.z * δt
-  dy = l^.x - l^.y + l^.x * l^.z * δt
-  dz = -3 * l^.z + l^.x * l^.y * δt
+wangEquation l = (dx*δt, dy*δt, dz*δt) where
+  dx = l^.x - l^.y * l^.z
+  dy = l^.x - l^.y + l^.x * l^.z
+  dz = -3   * l^.z + l^.x * l^.y
 
 duffingEquation :: SystemEquation
-duffingEquation l = (dx, dy, dz) where
-  dx = l^.y * δt
-  dy = -a * l^.y - (l^.x) ** 3 + b * cos(l^.z) * δt
-  dz = 1 * δt
+duffingEquation l = (dx*δt, dy*δt, dz*δt) where
+  dx = l^.y
+  dy = -a * l^.y - (l^.x) ** 3 + b * cos(l^.z)
+  dz = 1
   a = 0.1
   b = 11.0
 
 lotkaVolterraEquation :: SystemEquation
-lotkaVolterraEquation a = (dx, dy, dz) where
-  dx = a^.x * (1 - a^.x - 9 * a^.y) * δt
-  dy = -a^.y * (1 - 6*a^.x - a^.y + 9 * a^.z) * δt
-  dz = a^.z * (1 - 3 * a^.x - a^.z) * δt
+lotkaVolterraEquation a = (dx*δt, dy*δt, dz*δt) where
+  dx =  a^.x * ( 1 - 1 * a^.x - 9 * a^.y         )
+  dy = -a^.y * ( 1 - 6 * a^.x - a^.y + 9 * a^.z  )
+  dz =  a^.z * ( 1 - 3 * a^.x - a^.z             )
 
 chuaEquation :: SystemEquation
-chuaEquation a = (dx, dy, dz) where
-  dx = α * (a^.y - a^.x - ht) * δt
-  dy = a^.x - a^.y + a^.z * δt
-  dz = -ß * a^.y * δt
+chuaEquation a = (dx*δt, dy*δt, dz*δt) where
+  dx = α * (a^.y - a^.x - ht)
+  dy = a^.x - a^.y + a^.z
+  dz = -ß * a^.y
   ht = µ1 * a^.x + 0.5 * (µ0 - µ1) * ((abs $ a^.x + 1) - (abs $ a^.x - 1))
   α = 15.6
   ß = 28.0
